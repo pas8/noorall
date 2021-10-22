0aac00b4f72ca82f437afa7ebd079f54bbe4befb
pas8 <whyiampas@gmail.com>
2021-10-22 17:54:41 +0300

    _

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const body_node = document.querySelector('body');
 
-let primary_colors = ['#FAEBD7','#080808']
-let defaut_colors = [primary_colors[0], '#5F9EA0'];
+let primary_colors = ['#FAEBD7', '#080808'];
+let is_dark_theme = true;
+let defaut_colors = [primary_colors[+is_dark_theme], '#5F9EA0'];
 
 const set_up_colors = () => {
   body_node.style = `--noorall_primary:${defaut_colors[0]}; --noorall_secondary: ${defaut_colors[1]};--noorall_scroll:${defaut_colors[1]}`;
@@ -16,4 +17,18 @@ const color_preview_container_node = document.querySelector('#color_preview');
 
     set_up_colors();
   });
+});
+
+const theme_button_node = document.getElementById('_theme_button');
+
+theme_button_node.addEventListener('click', (el) => {
+  const path_node = document.getElementById('_theme_button').children[0].children[0];
+  const prev_d = path_node.getAttribute('d');
+  const current_d = path_node.getAttribute('_d');
+
+  path_node.setAttribute('d', current_d);
+  path_node.setAttribute('_d', prev_d);
+  is_dark_theme = !is_dark_theme;
+  defaut_colors[0] = primary_colors[+is_dark_theme];
+  set_up_colors();
 });
