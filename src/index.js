37d3ad75e9bed255ff9b982a66d9eb56338fa06e
pas8 <whyiampas@gmail.com>
2021-10-24 23:28:40 +0300

    !

--- src/index.js
+++ src/index.js
@@ -41,17 +41,21 @@ theme_button_node.addEventListener('click', () => {
 
   const random_id = () => Math.random().toString(32);
   var elements = [];
-  var recursiveFindChildren = function (el, parent_id) {
+
+  const recursive_find_children = function (el, parent_id) {
     for (var i = 0, iLength = el.children.length; i < iLength; i++) {
       const _id = random_id();
       elements.push({ el: el.children[i], _id, parent_id });
-      if (el.children[i].children.length) recursiveFindChildren(el.children[i], _id);
+      if (el.children[i].children.length) recursive_find_children(el.children[i], _id);
     }
+
     return elements;
   };
-  // codecontainer.
 
-  recursiveFindChildren(codecontainer);
+  const codecontainer_id = random_id();
+  recursive_find_children(codecontainer, codecontainer_id);
+
+  elements.push({ el: codecontainer, _id: codecontainer_id });
 
   el.addEventListener('click', async () => {
     try {
@@ -61,62 +65,39 @@ theme_button_node.addEventListener('click', () => {
     }
   });
 
-  let a = [];
-  let idx = 0;
-  let h = [];
-  const spacing = '&nbsp;&nbsp;&nbsp;&nbsp;';
-  const genegate_spacing = (length) => `${spacing}${spacing}${Array.from({ length }, () => spacing).join('')}`;
+  const spacing = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+  const genegate_spacing = (length) => `${spacing}&nbsp;&nbsp;&nbsp;&nbsp;${Array.from({ length }, () => spacing).join('')}`;
 
   const genegate_code_html = (arr) => {
     if (!arr?.length || !arr) return '';
-    return arr
-      .map(([{ tag_name, attributes, length, text, _arr }], i) => {
-        const is_end = arr.length === i + 1;
 
-        const end_code = is_end
-          ? arr
-              .filter(([{ length }]) => length != idx)
-              .reverse()
-              .map(([{ tag_name, length }]) => `${genegate_spacing(length)}<span __etag>${tag_name}</span>\n`)
-          : '';
+    return arr
+      .map(({ tag_name, attributes, children, text, _id }, i) => {
+        const length = nested_index_arr.find((__) => __._id === _id)?.idx;
 
         const start_code = `${genegate_spacing(length)}<span __stag>${tag_name}<i>${attributes
           .map(([key, value]) => (!value ? ` ${key}` : ` ${key}='${value}'`))
           .join(' ')}`;
-        const h = length > arr[i + 1]?.[0]?.length;
-
         const check_text_content = !!text?.trim() ? `${genegate_spacing(length + 1)}<span>${text}</span>\n` : '';
-        const check_of_end_tag =
-          length === idx || h ? `${genegate_spacing(length)}<span __etag>${tag_name}</span>\n` : '';
+        const check_of_end_tag = `${genegate_spacing(length)}<span __etag>${tag_name}</span>\n`;
 
-        // console.log(tag_name,length ,  arr[i+1]?.[0]?.length)
-
-        return `${start_code} </i></span>\n${check_text_content} ${check_of_end_tag}${
-          !!_arr.length ? genegate_code_html(_arr) : ''
-        } ${end_code}`;
+        return `${start_code} </i></span>\n${check_text_content}${genegate_code_html(children)} ${check_of_end_tag}`;
       })
       .join('');
   };
 
-  const set_up_item_propertyies = (el, arr, _arr) => {
+  const root = [];
+  const flat = [...elements].map(({ el, ...props }) => {
     const attributes = Object.values({ ...el.attributes }).map((__) => [__.name, __.value]);
     const text = el.childNodes[0].nodeValue || '';
     const tag_name = el?.tagName?.toLowerCase();
 
-    arr.push([{ tag_name, attributes, length: idx, text, _arr }]);
-    const children_count = [...el.children]?.length;
-    return children_count;
-  };
-
-  // Create root for top-level node(s)
-  const root = [];
-  const flat = [...elements];
+    return { ...props, tag_name, text, attributes };
+  });
 
   flat.forEach((node) => {
-    // No parentId means top level
     if (!node.parent_id) return root.push(node);
 
-    // Insert node as child of parent in flat array
     const parentIndex = flat.findIndex((el) => el._id === node.parent_id);
     if (!flat[parentIndex].children) {
       return (flat[parentIndex].children = [node]);
@@ -125,59 +106,27 @@ theme_button_node.addEventListener('click', () => {
     flat[parentIndex].children.push(node);
   });
 
+  let nested_index_arr = [];
 
-  const get_recursionion_children_props = (el, i, arr, _arr = []) => {
-    // console.log(arr);
-    // recursiveFindChildren(el)
-
-    if (!arr || !el) return;
-    idx = i;
-    if (![el.children].length) return;
-
-    const children_count = set_up_item_propertyies(el, arr, _arr);
-    // console.log(children_count)
-    // children_count
-
-    if (children_count <= 1) return get_recursionion_children_props(el.children[0], i + 1, arr, _arr);
-
-    return [...el.children].forEach((__, _i) => {
-      let __arr = [];
-      // let _idx = 0;
-      // _idx = i;
-      // get_recursionion_children_props(__, idx, __arr,arr);
-      // if (!__arr.length) {
-      // get_recursionion_children_props(__, idx, __arr,arr);
-      // } else {
-      // get_recursionion_children_props(__, idx, arr,__arr);
-
-      // console.log(';',__arr);
-      // }
-      // console.log(arr,0, __arr)
-      // get_recursionion_children_props(__, 0,arr, __arr);
-      // console.log(__arr,arr)
-
-      // if (!!__arr.length) {
-      // get_recursionion_children_props(el, idx, arr, __arr);
-      // }
-      // if (!!_arr.length) {
-      //   let __arr = [] ;
-      //   get_recursionion_children_props(__, _idx, __arr, _arr);
-      // }
-      // console.log(_i, i, idx, _arr);
-      // get_recursionion_children_props(, idx, arr, _arr);
-      // console.log(_arr,_idx);
-      // set_up_item_propertyies(__, _arr);
-      // const code = genegate_code_html(_arr);
-      // console.log(_arr)
-      // console.log(_arr,arr)
-      // console.log(_arr,_idx);
-      // return arr.push({ code });
+  const get_nested_idx_and_push = (el, idx) => {
+    if (!el.children?.length) {
+      nested_index_arr.push({ _id: el._id, idx });
       return;
-    });
+    }
+    nested_index_arr.push({ _id: el._id, idx });
+
+    return iterate_items_to_find_nested_idx(el.children, idx + 1);
   };
 
-  get_recursionion_children_props(codecontainer, 0, a);
-  const code_html = genegate_code_html(a);
-  // console.log(a);
+  const iterate_items_to_find_nested_idx = (arr, idx = 0) => {
+    if (!arr) return;
+
+    arr.forEach((el) => {
+      get_nested_idx_and_push(el, idx);
+    });
+  };
+  iterate_items_to_find_nested_idx(root);
+
+  const code_html = genegate_code_html(root);
   el.innerHTML = `<code>\n${code_html}\n</code>`;
 });
