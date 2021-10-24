ccf6a03623640ac975fd39f4ebcd918e1737678a
pas8 <whyiampas@gmail.com>
2021-10-24 17:06:18 +0300

    =

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const color_preview_container_node = document.querySelector('#color_preview');
 });
 const theme_button_node = document.getElementById('_theme_button');
 
-theme_button_node.addEventListener('click', (el) => {
+theme_button_node.addEventListener('click', () => {
   const path_node = document.getElementById('_theme_button').children[0].children[0];
   const prev_d = path_node.getAttribute('d');
   const current_d = path_node.getAttribute('_d');
@@ -39,49 +39,52 @@ theme_button_node.addEventListener('click', (el) => {
   const __id = el.getAttribute('__id');
   const codecontainer = document.getElementById(__id);
 
+  el.addEventListener('click', async () => {
+    try {
+      navigator.clipboard.writeText(codecontainer.outerHTML);
+    } catch (error) {
+      console.error('Async: Could not copy text: ', err);
+    }
+  });
+
   let arr = [];
   let idx = 0;
 
-  const j = (el, i) => {
+  const get_recursionion_children_props = (el, i) => {
     idx = i;
     const attributes = Object.values({ ...el.attributes }).map((__) => [__.name, __.value]);
-    arr.push([{ tag_name: el?.tagName?.toLowerCase(), attributes, length: idx }]);
-
+    const text = el.childNodes[0].nodeValue || '';
+    arr.push([{ tag_name: el?.tagName?.toLowerCase(), attributes, length: idx, text }]);
     if (![...el.children]?.length) return;
 
     [...el.children].forEach((el) => {
-      j(el, i + 1);
+      get_recursionion_children_props(el, i + 1);
     });
   };
 
-  j(codecontainer, 0);
+  get_recursionion_children_props(codecontainer, 0);
 
   const spacing = '&nbsp;&nbsp;&nbsp;&nbsp;';
   const genegate_spacing = (length) => `${spacing}${spacing}${Array.from({ length }, () => spacing).join('')}`;
 
-  const g = arr
+  const code_html = arr
     .map(
-      ([{ tag_name, attributes, length }], i) =>
+      ([{ tag_name, attributes, length, text }], i) =>
         `${genegate_spacing(length)}<span __stag>${tag_name} <i>${attributes
           .map(([key, value]) => (!value ? key : `${key}='${value}'`))
-          .join(' ')} </i></span>\n${
+          .join(' ')} </i></span>\n${!!text?.trim() ? `${genegate_spacing(length + 1)}<span>${text}</span>\n` : ''} ${
           length === idx ? `${genegate_spacing(length)}<span __etag>${tag_name}</span>\n` : ''
         } ${
           arr.length === i + 1
             ? arr
                 .filter(([{ length }]) => length != idx)
                 .map(
-                  ([{ tag_name, length }]) =>
-                    `${genegate_spacing(idx - length -1)}<span __etag>${tag_name}</span>\n`
+                  ([{ tag_name, length }]) => `${genegate_spacing(idx - length - 1)}<span __etag>${tag_name}</span>\n`
                 )
             : ''
         }`
     )
     .join('');
 
-  el.innerHTML = `<code>\n${g}\n</code>`;
-  console.log(g, idx, arr.length);
-  // el.innerHTML = `<code>${outer_html}</code>`;
+  el.innerHTML = `<code>\n${code_html}\n</code>`;
 });
-
-// const h = [...(document.getElementsByTagName('pre') || [])].map((el) => el.children[0]);
